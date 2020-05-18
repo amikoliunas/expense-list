cd53e248e642694eb8aad294f6532113b1195ea6
ljagintavicius <64728247+ljagintavicius@users.noreply.github.com>
2020-05-18 17:15:14 +0300

    ExpenseController su [HttpPost] Post() metodu
    Sukūriau metodą, kuris įkelia duomenis į db. ExpenseItem objektą sukūriau pačiam metodu, bet čia tik pradžiai, pasibandymui ar veikia.

--- Controllers/ExpenseController.cs
+++ Controllers/ExpenseController.cs
@@ -1,29 +1,61 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 
 namespace ExpensesWebApp.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+
     public class ExpenseController : ControllerBase
+
     {
         private readonly ILogger<ExpenseController> _logger;
+        public IConfiguration Configuration { get; }
 
-        public ExpenseController (ILogger<ExpenseController> logger)
+        public ExpenseController (ILogger<ExpenseController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            Configuration = configuration;
         }
-
+        
         [HttpGet]
-        public IEnumerable<Expense> Get()
+        public IEnumerable<ExpenseItem> Get()
         {
-            return Enumerable.Range(1, 1).Select(index => new Expense
+            return Enumerable.Range(1, 1).Select(index => new ExpenseItem
             {
-                ExpenseSum = 10
+                Expenses = 10
             })
             .ToArray();
+        }
+        [HttpPost]
+        public void Post()
+        {
+            string connStr = Configuration.GetConnectionString("DatabaseConnectionString");
+            using var conn = new SqlConnection(connStr);
+            conn.Open();
+            ExpenseItem one = new ExpenseItem
+            {
+                ExpenseDate = new DateTime(2000, 1, 1),
+                Expenses = 20.01,
+                Expense_type_ID = 2,
+                User_ID = 1
+            };
+            SqlCommand useDB = new SqlCommand("USE Expenses", conn);
+            useDB.ExecuteNonQuery();
+            string query = "INSERT INTO dbo.expenses (date, expenses, expense_type_ID, user_ID) ";
+            query += "VALUES (@date, @expenses, @expense_type_ID, @user_ID)";
+            SqlCommand insert = new SqlCommand(query, conn);
+            insert.Parameters.AddWithValue("@date", one.ExpenseDate);
+            insert.Parameters.AddWithValue("@expenses", one.Expenses);
+            insert.Parameters.AddWithValue("@expense_type_ID", one.Expense_type_ID);
+            insert.Parameters.AddWithValue("@user_ID", one.User_ID);
+            insert.ExecuteNonQuery();
+            conn.Close();
         }
 
     }
---
+++ Expense.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpensesWebApp
+{
+    public class ExpenseItem
+    {
+        public DateTime ExpenseDate { get; set; }
+        public double Expenses { get; set; }
+        public int Expense_type_ID { get; set; }
+        public int User_ID { get; set; }
+    }
+}
